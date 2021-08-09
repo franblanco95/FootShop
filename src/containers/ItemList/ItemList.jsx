e60fc4b056d70d2627675760fa917a6b7cdc76ed
Francisco Blanco <fran_boca@hotmail.com>
2021-08-09 13:49:35 -0300

    Agregue funcion para categorias con mayusculas

--- src/containers/ItemList/ItemList.jsx
+++ src/containers/ItemList/ItemList.jsx
@@ -4,11 +4,16 @@ import { CardComponent } from "../../components/CardComponent/CardComponent"
 import { Col, Container, Row } from 'react-bootstrap'
 
 export function ItemList({ productos, category }) {
+
+    function mayusc({ category }) {
+        return category.charAt(0).toUpperCase() + category.slice(1)
+    }
+
     return (
         <>
             <Container fluid className="list-fondo p-5">
                 <Row>
-                    <h2 className="list-category">{category}</h2>
+                    <h2 className="list-category">{mayusc({ category })}</h2>
                     <Col className="list-row">
                         {productos.map((element) => {
                             return (
